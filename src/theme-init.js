0543543bb9ea7026ba917b03383cf99232768f7e
Lucian Pacurar <codesensedev@outlook.com>
2026-05-03 07:44:05 -0500

    updates

--- src/theme-init.js
+++
@@ -1,1 +0,0 @@
-try { var _t = localStorage.getItem('lpdf-theme'); if (_t) document.documentElement.setAttribute('data-theme', _t); } catch(e) {}
--- www/assets/js/theme-init.js
+++
@@ -1,1 +0,0 @@
-try{var _t=localStorage.getItem("lpdf-theme");_t&&document.documentElement.setAttribute("data-theme",_t)}catch{}
